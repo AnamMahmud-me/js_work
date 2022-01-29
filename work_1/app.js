38ca99bd7bda87b516f2f74961fd05ed975d4b88
anammahmud131 <anammahmud131@gmail.com>
2022-01-29 22:09:22 +0600

    first commit

---
+++ work_1/app.js
@@ -0,0 +1,12 @@
+function low(){
+  document.getElementById('image').src='img/low.png';
+}
+function medium(){
+  document.getElementById('image').src='img/medium.png';
+}
+function almostFull(){
+  document.getElementById('image').src='img/almost_full.png';
+}
+function full(){
+  document.getElementById('image').src='img/full.png';
+}
